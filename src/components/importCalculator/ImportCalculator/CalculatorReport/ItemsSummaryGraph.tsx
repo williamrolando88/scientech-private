af87fac3f1f9a5dc059957699857f1dd2386f907
William Morales <william.rolando88@hotmail.com>
2023-12-25 21:05:41 -0500

    Update chart styles

--- src/components/importCalculator/ImportCalculator/CalculatorReport/ItemsSummaryGraph.tsx
+++ src/components/importCalculator/ImportCalculator/CalculatorReport/ItemsSummaryGraph.tsx
@@ -1,9 +1,37 @@
 import { FC } from 'react';
-import Chart from 'src/components/shared/chart';
+import Chart, { useChart } from 'src/components/shared/chart';
 import { useImportCalculatorContext } from 'src/hooks/useImportCalculatorContext';
 
 const ItemsSummaryGraph: FC = () => {
   const { values, reportValues } = useImportCalculatorContext();
+
+  const chartOptions = useChart({
+    chart: {
+      type: 'bar',
+      stacked: true,
+      stackType: '100%',
+      height: '20px',
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      },
+    },
+    xaxis: {
+      categories: values.items.map((item) => item.name),
+    },
+    tooltip: {
+      y: {
+        formatter(val) {
+          return `$ ${val}`;
+        },
+      },
+    },
+    legend: {
+      position: 'top',
+      horizontalAlign: 'center',
+    },
+  });
 
   if (!reportValues.length) return null;
 
@@ -13,37 +41,7 @@ const ItemsSummaryGraph: FC = () => {
         height={values.items.length * 40 + 120}
         type="bar"
         series={reportValues}
-        options={{
-          chart: {
-            type: 'bar',
-            stacked: true,
-            stackType: '100%',
-            height: '20px',
-          },
-          plotOptions: {
-            bar: {
-              horizontal: true,
-            },
-          },
-          xaxis: {
-            categories: values.items.map((item) => item.name),
-          },
-          tooltip: {
-            y: {
-              formatter(val) {
-                return `$ ${val}`;
-              },
-            },
-          },
-          title: {
-            text: 'Reporte de costos por artículo',
-            align: 'center',
-          },
-          legend: {
-            position: 'top',
-            horizontalAlign: 'center',
-          },
-        }}
+        options={chartOptions}
       />
     </div>
   );
