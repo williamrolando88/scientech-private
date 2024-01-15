17fa7e73dc7ea48646f2adbd1ad113577825d512
William Morales <william.rolando88@hotmail.com>
2024-01-14 22:10:09 -0500

    Add regex check to account id field

--- src/lib/parsers/accountCategories.ts
+++ src/lib/parsers/accountCategories.ts
@@ -1,8 +1,12 @@
 import { z } from 'zod';
+import { ZOD_ERROR } from '../constants/errors';
+import { ACCOUNT_CATEGORY_REGEX } from '../constants/regex';
 
 export const AccountCategoryParser = z.object({
-  id: z.string(),
-  name: z.string(),
+  id: z
+    .string(ZOD_ERROR.REQUIRED)
+    .regex(ACCOUNT_CATEGORY_REGEX, ZOD_ERROR.ACCOUNT_CATEGORY_ID),
+  name: z.string(ZOD_ERROR.REQUIRED),
   editable: z.boolean(),
   description: z.string().optional(),
 });
