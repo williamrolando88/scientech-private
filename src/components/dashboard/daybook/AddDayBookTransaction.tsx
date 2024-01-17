488b236d85bde845afa908fff1e79b1e118f2ed2
William Morales <william.rolando88@hotmail.com>
2024-01-17 07:44:02 -0500

    Add reusable formik textfield component

--- src/components/dashboard/daybook/AddDayBookTransaction.tsx
+++ src/components/dashboard/daybook/AddDayBookTransaction.tsx
@@ -1,5 +1,6 @@
 import {
   Alert,
+  Box,
   Button,
   Card,
   Dialog,
@@ -9,26 +10,15 @@ import {
   Stack,
   TextField,
 } from '@mui/material';
+import { Form, Formik } from 'formik';
 import { FC, useState } from 'react';
+import { FormikTextField } from 'src/components/shared/formik-components';
 import Iconify from 'src/components/shared/iconify';
-import { DAYBOOK_TRANSACTION_DETAIL_INITIAL_VALUE } from 'src/lib/constants/dayBook';
-import { DayBookTransactionDetail } from 'src/types/dayBook';
 
 const AddDayBookTransaction: FC = () => {
   const [openModal, setOpenModal] = useState(false);
-  const [transactions, setTransactions] = useState<DayBookTransactionDetail[]>(
-    new Array(2).fill(DAYBOOK_TRANSACTION_DETAIL_INITIAL_VALUE)
-  );
-
   const handleOpenModal = () => setOpenModal(true);
   const handleCloseModal = () => setOpenModal(false);
-  const handleAddTransaction = () =>
-    setTransactions([
-      ...transactions,
-      DAYBOOK_TRANSACTION_DETAIL_INITIAL_VALUE,
-    ]);
-  const handleDeleteTransaction = (index: number) =>
-    setTransactions(transactions.filter((_, i) => i !== index));
 
   return (
     <>
@@ -44,48 +34,65 @@ const AddDayBookTransaction: FC = () => {
       >
         <DialogTitle>Nuevo asiento contable</DialogTitle>
 
-        <Stack component={DialogContent} gap={2}>
-          <Alert severity="info">
-            Aqui va el formulario de asiento contable
-          </Alert>
-
-          <Stack>
-            <TextField label="Fecha" />
-          </Stack>
-
-          <Stack component={Card} variant="outlined" p={2}>
-            {transactions.map((transaction, index) => (
-              <Stack key={index} direction="row">
-                <TextField label="Cuenta" />
-                <TextField label="Descripción cuenta" />
-                <TextField label="Debe" />
-                <TextField label="Haber" />
-                <TextField label="Descripción de la Transacción" />
-                <TextField label="Cotización" />
-                <TextField label="Factura" />
-                <Button
-                  variant="outlined"
-                  color="error"
-                  onClick={() => handleDeleteTransaction(index)}
-                >
-                  <Iconify icon="pajamas:remove" />
-                </Button>
-              </Stack>
-            ))}
-
-            <Button variant="soft" onClick={handleAddTransaction}>
-              Agregar
-            </Button>
-          </Stack>
-        </Stack>
-
-        <DialogActions>
-          <Button onClick={handleCloseModal}>Cancelar</Button>
-          <Button>Guardar</Button>
-        </DialogActions>
+        <AddDayBookTransactionForm />
       </Dialog>
     </>
   );
 };
 
 export default AddDayBookTransaction;
+
+const AddDayBookTransactionForm = () => {
+  console.log('first');
+
+  return (
+    <Formik initialValues={{}} onSubmit={() => {}}>
+      {() => (
+        <Form>
+          <Stack component={DialogContent} gap={2}>
+            <Alert severity="info">
+              Aqui va el formulario de asiento contable
+            </Alert>
+
+            <Stack>
+              <TextField label="Fecha" />
+            </Stack>
+
+            <Stack component={Card} variant="outlined" p={2}>
+              <Stack direction="row">
+                <Box>Cuenta</Box>
+                <Box>Descripción cuenta</Box>
+                <Box>Debe</Box>
+                <Box>Haber</Box>
+                <Box>Descripción de la Transacción</Box>
+                <Box>Cotización</Box>
+                <Box>Factura</Box>
+              </Stack>
+
+              <Stack direction="row">
+                <FormikTextField name="account" />
+                <TextField />
+                <TextField />
+                <TextField />
+                <TextField />
+                <TextField />
+                <TextField />
+                <TextField />
+                <Button variant="outlined" color="error">
+                  <Iconify icon="pajamas:remove" />
+                </Button>
+              </Stack>
+
+              <Button variant="soft">Agregar</Button>
+            </Stack>
+          </Stack>
+
+          <DialogActions>
+            <Button>Cancelar</Button>
+            <Button>Guardar</Button>
+          </DialogActions>
+        </Form>
+      )}
+    </Formik>
+  );
+};
---
+++ src/components/shared/formik-components/index.ts
@@ -0,0 +1,1 @@
+export { FormikTextField } from './FormikTextField';
