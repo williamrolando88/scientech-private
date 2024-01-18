bd5c7081884cbf5612ee480d2e7491130cb0d40a
William Morales <william.rolando88@hotmail.com>
2024-01-18 18:57:34 -0500

    Add FormikAutocalculateInput component

---
+++ src/components/shared/formik-components/FormikAutoCalculateField.tsx
@@ -0,0 +1,38 @@
+import { useFormikContext } from 'formik';
+import { get } from 'lodash';
+import { FC } from 'react';
+import {
+  AutoCalculateInput,
+  AutoCalculateInputProps,
+} from '../AutoCalculateInput';
+
+interface FormikAutoCalculateFieldProps
+  extends Omit<AutoCalculateInputProps, 'value' | 'error' | 'helperText'> {
+  name: string;
+}
+
+const FormikAutoCalculateField: FC<FormikAutoCalculateFieldProps> = (props) => {
+  const { name } = props;
+  const { values, errors, touched, setFieldValue } = useFormikContext();
+
+  const fieldValue = get(values, name, '');
+  const fieldError = get(errors, name, '');
+  const fieldTouched = get(touched, name, false);
+
+  const handleChange = (inputName: string, inputValue: number) => {
+    setFieldValue(inputName, inputValue);
+  };
+
+  return (
+    <AutoCalculateInput
+      {...props}
+      onFocus={(e) => e.target.select()}
+      value={fieldValue}
+      onChange={handleChange}
+      error={fieldTouched && !!fieldError}
+      helperText={fieldTouched && fieldError}
+    />
+  );
+};
+
+export default FormikAutoCalculateField;
