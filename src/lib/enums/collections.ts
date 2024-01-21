025c4da8b518e74590dafdbb0ae32e221519fe52
William Morales <william.rolando88@hotmail.com>
2024-01-21 16:58:59 -0500

    Add dayBookTransactions collections

--- src/lib/enums/collections.ts
+++ src/lib/enums/collections.ts
@@ -2,6 +2,7 @@ export enum COLLECTIONS {
   IMPORT_CALCULATIONS = 'importCalculations',
   USERS = 'users',
   APPLICATION_SETTINGS = 'applicationSettings',
+  DAY_BOOK_TRANSACTIONS = 'dayBookTransactions',
 }
 
 export enum APPLICATION_SETTINGS {
