53eb348a123861b6479ca95c9bdc8e260c5a60a3
William Morales <william.rolando88@hotmail.com>
2024-01-28 20:19:14 -0500

    Add db persistor

---
+++ src/lib/constants/reactQuery.ts
@@ -0,0 +1,5 @@
+export const REACT_QUERY = {
+  PERSISTOR_DB: 'REACT_QUERY_OFF_CACHE',
+  STALE_TIME: 12 * 60 * 60 * 1000, // 12 hours
+  GC: 12 * 60 * 60 * 1000, // 12 hours
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { NextPage } from 'next';
 import { AppProps } from 'next/app';
@@ -15,7 +16,9 @@ import 'slick-carousel/slick/slick.css';
 import { MotionLazyContainer } from 'src/components/shared/animate';
 import { StyledChart } from 'src/components/shared/chart';
 import SnackbarProvider from 'src/components/shared/snackbar/SnackbarProvider';
+import { REACT_QUERY } from 'src/lib/constants/reactQuery';
 import ThemeProvider from 'src/lib/theme';
+import { createIDBPersister } from 'src/lib/utils/idbPersister';
 import { AuthProvider } from 'src/services/auth/FirebaseContext';
 import 'yet-another-react-lightbox/plugins/captions.css';
 import 'yet-another-react-lightbox/plugins/thumbnails.css';
@@ -42,39 +45,47 @@ export default function MyApp(props: MyAppProps) {
 
   const queryClient = new QueryClient({
     defaultOptions: {
-      queries: { staleTime: 6 * 60 * 60 * 1000 },
+      queries: {
+        staleTime: REACT_QUERY.STALE_TIME,
+        gcTime: REACT_QUERY.GC,
+        networkMode: 'offlineFirst',
+      },
     },
   });
 
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{
+        persister: createIDBPersister(REACT_QUERY.PERSISTOR_DB),
+      }}
+    >
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
 
-      <AuthProvider>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <SettingsProvider>
-            <MotionLazyContainer>
-              <ThemeProvider>
-                <ThemeSettings>
-                  <SnackbarProvider>
-                    <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <SettingsProvider>
+              <MotionLazyContainer>
+                <ThemeProvider>
+                  <ThemeSettings>
+                    <SnackbarProvider>
                       <StyledChart />
 
                       <ProgressBar />
 
                       {getLayout(<Component {...pageProps} />)}
-
-                      <ReactQueryDevtools buttonPosition="bottom-left" />
-                    </QueryClientProvider>
-                  </SnackbarProvider>
-                </ThemeSettings>
-              </ThemeProvider>
-            </MotionLazyContainer>
-          </SettingsProvider>
-        </LocalizationProvider>
-      </AuthProvider>
-    </CacheProvider>
+                    </SnackbarProvider>
+                  </ThemeSettings>
+                </ThemeProvider>
+              </MotionLazyContainer>
+            </SettingsProvider>
+          </LocalizationProvider>
+        </AuthProvider>
+      </CacheProvider>
+      <ReactQueryDevtools buttonPosition="bottom-left" />
+    </PersistQueryClientProvider>
   );
 }
