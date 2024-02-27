0ce96d4ddabaee7a1b785e0d5e40ffb6490902ab
William Morales <william.rolando88@hotmail.com>
2024-02-26 21:27:51 -0500

    Add new app routes

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -10,22 +10,45 @@ export const PATH_AUTH = {
 
 export const PATH_DASHBOARD = {
   root: ROOTS_DASHBOARD,
-  home: path(ROOTS_DASHBOARD, '/home'),
+  home: path(ROOTS_DASHBOARD, 'home'),
   calculator: {
-    root: path(ROOTS_DASHBOARD, '/import-calculator'),
-    list: path(ROOTS_DASHBOARD, '/import-calculator/list'),
-    new: path(ROOTS_DASHBOARD, '/import-calculator/new'),
-    view: (id: string) => path(ROOTS_DASHBOARD, `/import-calculator/${id}`),
+    root: path(ROOTS_DASHBOARD, 'import-calculator'),
+    list: path(ROOTS_DASHBOARD, 'import-calculator', 'list'),
+    new: path(ROOTS_DASHBOARD, 'import-calculator', 'new'),
+    open: (id: string) => path(ROOTS_DASHBOARD, 'import-calculator', id),
   },
-
+  client: {
+    root: path(ROOTS_DASHBOARD, 'client'),
+    list: path(ROOTS_DASHBOARD, 'client', 'list'),
+    new: path(ROOTS_DASHBOARD, 'client', 'new'),
+    open: (id: string) => path(ROOTS_DASHBOARD, 'client', id),
+  },
+  projects: {
+    root: path(ROOTS_DASHBOARD, 'projects'),
+    list: path(ROOTS_DASHBOARD, 'projects', 'list'),
+    new: path(ROOTS_DASHBOARD, 'projects', 'new'),
+    open: (id: string) => path(ROOTS_DASHBOARD, 'projects', id),
+  },
+  receivedInvoices: {
+    root: path(ROOTS_DASHBOARD, 'received-invoices'),
+    list: path(ROOTS_DASHBOARD, 'received-invoices', 'list'),
+    new: path(ROOTS_DASHBOARD, 'received-invoices', 'new'),
+    open: (id: string) => path(ROOTS_DASHBOARD, 'received-invoices', id),
+  },
+  issuedInvoices: {
+    root: path(ROOTS_DASHBOARD, 'issued-invoices'),
+    list: path(ROOTS_DASHBOARD, 'issued-invoices', 'list'),
+    new: path(ROOTS_DASHBOARD, 'issued-invoices', 'new'),
+    open: (id: string) => path(ROOTS_DASHBOARD, 'issued-invoices', id),
+  },
   dayBook: {
-    root: path(ROOTS_DASHBOARD, '/day-book'),
+    root: path(ROOTS_DASHBOARD, 'day-book'),
   },
   accountCategories: {
-    root: path(ROOTS_DASHBOARD, '/account-categories'),
+    root: path(ROOTS_DASHBOARD, 'account-categories'),
   },
   documentParser: {
-    root: path(ROOTS_DASHBOARD, '/document-parser'),
-    invoice: path(ROOTS_DASHBOARD, '/document-parser/invoice'),
+    root: path(ROOTS_DASHBOARD, 'document-parser'),
+    invoice: path(ROOTS_DASHBOARD, 'document-parser', 'invoice'),
   },
 };
