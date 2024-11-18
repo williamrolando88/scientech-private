280fc0d8919f1843043137644cbaaed6cc9a1fa4
William Morales <william.morales@devsu.com>
2024-11-18 13:41:48 -0500

    Fix linter errors

--- src/hooks/cache/expenses.ts
+++ src/hooks/cache/expenses.ts
@@ -1,7 +1,7 @@
 import { COLLECTIONS } from '@src/lib/enums/collections';
 import { Expenses } from '@src/services/firebase/expenses';
-import { ExpenseTypeValuesOld, GeneralExpenseOld } from '@src/types/expenses';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { ExpenseTypeValuesOld } from '@src/types/expenses';
+import { useQuery } from '@tanstack/react-query';
 
 const queryKeyByType = (type: ExpenseTypeValuesOld) => [
   COLLECTIONS.EXPENSES,
@@ -18,99 +18,3 @@ export function useListExpensesByType<T>(type: ExpenseTypeValuesOld) {
 
   return { ...query, data: (query.data ?? []) as T[] };
 }
-
-export const useAddExpenseByType = (type: ExpenseTypeValuesOld) => {
-  const queryClient = useQueryClient();
-
-  const mutation = useMutation({
-    mutationFn: Expenses.upsert,
-    onMutate: async () => {
-      await queryClient.cancelQueries({ queryKey: queryKeyByType(type) });
-      await queryClient.cancelQueries({ queryKey: [COLLECTIONS.PROJECTS] });
-      await queryClient.cancelQueries({
-        queryKey: [COLLECTIONS.DAY_BOOK_TRANSACTIONS],
-      });
-    },
-    onSuccess: (newExpense) => {
-      queryClient.setQueryData(
-        queryKeyByType(type),
-        (prevData: GeneralExpenseOld[]) => [...(prevData ?? []), newExpense],
-      );
-    },
-    onError: () => {
-      queryClient.invalidateQueries({ queryKey: queryKeyByType(type) });
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries({ queryKey: [COLLECTIONS.PROJECTS] });
-      queryClient.invalidateQueries({
-        queryKey: [COLLECTIONS.DAY_BOOK_TRANSACTIONS],
-      });
-    },
-  });
-
-  return mutation;
-};
-
-export const useUpdateExpenseByType = (type: ExpenseTypeValuesOld) => {
-  const queryClient = useQueryClient();
-
-  const mutation = useMutation({
-    mutationFn: Expenses.upsert,
-    onMutate: async () => {
-      await queryClient.cancelQueries({ queryKey: queryKeyByType(type) });
-      await queryClient.cancelQueries({ queryKey: [COLLECTIONS.PROJECTS] });
-      await queryClient.cancelQueries({
-        queryKey: [COLLECTIONS.DAY_BOOK_TRANSACTIONS],
-      });
-    },
-    onSuccess: (newExpense) => {
-      queryClient.setQueryData(
-        queryKeyByType(type),
-        (prevData: GeneralExpenseOld[]) =>
-          prevData.map((expense) =>
-            expense.id === newExpense.id ? newExpense : expense,
-          ),
-      );
-    },
-    onError: () => {
-      queryClient.invalidateQueries({ queryKey: queryKeyByType(type) });
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries({ queryKey: [COLLECTIONS.PROJECTS] });
-      queryClient.invalidateQueries({
-        queryKey: [COLLECTIONS.DAY_BOOK_TRANSACTIONS],
-      });
-    },
-  });
-
-  return mutation;
-};
-
-export const useDeleteExpenseByType = (type: ExpenseTypeValuesOld) => {
-  const queryClient = useQueryClient();
-
-  const mutation = useMutation({
-    mutationFn: Expenses.remove,
-    onMutate: async () => {
-      await queryClient.cancelQueries({ queryKey: queryKeyByType(type) });
-    },
-    onSuccess: (id) => {
-      queryClient.setQueryData(
-        queryKeyByType(type),
-        (prevData: GeneralExpenseOld[]) =>
-          prevData.filter((expense) => expense.id !== id),
-      );
-    },
-    onError: () => {
-      queryClient.invalidateQueries({ queryKey: queryKeyByType(type) });
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries({ queryKey: [COLLECTIONS.PROJECTS] });
-      queryClient.invalidateQueries({
-        queryKey: [COLLECTIONS.DAY_BOOK_TRANSACTIONS],
-      });
-    },
-  });
-
-  return mutation;
-};
--- src/services/firebase/expenses/index.ts
+++ src/services/firebase/expenses/index.ts
@@ -1,13 +1,8 @@
-import { PROJECTS_INITIAL_VALUE } from '@src/lib/constants/projects';
 import { COLLECTIONS } from '@src/lib/enums/collections';
 import { converterByType } from '@src/services/firebase/expenses/converters';
-import { docReferencer, previousProjectReferencer } from '@src/services/firebase/expenses/docReferencer';
-import { ExpenseParserByType } from '@src/services/firebase/expenses/parsers';
 import { DB } from '@src/settings/firebase';
-import { DayBookTransactionDetailOld, DayBookTransactionOld } from '@src/types/dayBook';
-import { ExpenseOld, ExpenseTypeValuesOld, ExtendedGeneralExpenseOld, GeneralExpenseOld } from '@src/types/expenses';
-import { Project } from '@src/types/projects';
-import { collection, getDocs, query, runTransaction, where } from 'firebase/firestore';
+import { ExpenseTypeValuesOld } from '@src/types/expenses';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 
 const listByType = (type: ExpenseTypeValuesOld) => {
   const converter = converterByType[type];
@@ -15,7 +10,7 @@ const listByType = (type: ExpenseTypeValuesOld) => {
   return async () => {
     const q = query(
       collection(DB, COLLECTIONS.EXPENSES),
-      where('type', '==', type),
+      where('type', '==', type)
     ).withConverter(converter);
 
     const querySnapshot = await getDocs(q);
@@ -26,133 +21,6 @@ const listByType = (type: ExpenseTypeValuesOld) => {
   };
 };
 
-async function upsert(
-  expense: ExtendedGeneralExpenseOld,
-): Promise<GeneralExpenseOld> {
-  const timestamp = new Date();
-
-  const { dayBookDocRef, expenseDocRef, projectDocRef } =
-    docReferencer(expense);
-
-  const transactions: DayBookTransactionDetailOld[] =
-    expense.transaction_details.map((transaction) => ({
-      ...transaction,
-      expense_id: expenseDocRef.id,
-      project_id: projectDocRef?.id ?? '',
-    }));
-
-  let newExpense: ExpenseOld = {
-    issue_date: expense.issue_date,
-    total: expense.total,
-    type: expense.type,
-  };
-
-  try {
-    await runTransaction(DB, async (transaction) => {
-      // Read operations
-      const storedDayBookDoc = await transaction.get(dayBookDocRef);
-      const storedProjectDoc =
-        projectDocRef && (await transaction.get(projectDocRef));
-      const { previousProjectDoc } = await previousProjectReferencer(
-        transaction,
-        expenseDocRef,
-      );
-
-      /*
-       * Data manipulation
-       */
-
-      // Daybook manipulation
-      const newDayBookDoc: DayBookTransactionOld = {
-        date: expense.issue_date,
-        transactions,
-        createdAt: timestamp,
-        id: dayBookDocRef.id,
-        locked: true,
-        updatedAt: timestamp,
-      };
-
-      if (storedDayBookDoc.exists()) {
-        newDayBookDoc.createdAt = storedDayBookDoc.data().createdAt;
-      }
-
-      // Previous project manipulation
-      let previousProject: Project = PROJECTS_INITIAL_VALUE;
-      if (previousProjectDoc) {
-        previousProject = previousProjectDoc.data();
-        previousProject.received_vouchers =
-          previousProject.received_vouchers.filter(
-            (voucher: { id: string; }) => voucher.id !== expenseDocRef.id,
-          );
-      }
-
-      // Project manipulation
-      let newProject: Project = PROJECTS_INITIAL_VALUE;
-      if (projectDocRef && storedProjectDoc?.exists()) {
-        newProject = storedProjectDoc?.data();
-        newProject.received_vouchers = [
-          ...newProject.received_vouchers,
-          {
-            id: expenseDocRef.id,
-            type: expense.type,
-          },
-        ];
-      }
-
-      newExpense = { ...ExpenseParserByType[expense.type].parse(expense) };
-      newExpense.day_book_transaction_id = dayBookDocRef.id;
-      newExpense.id = expenseDocRef.id;
-
-      // Set operations
-      transaction.set(dayBookDocRef, newDayBookDoc);
-      transaction.set(expenseDocRef, newExpense);
-      if (projectDocRef && storedProjectDoc?.exists()) {
-        transaction.set(projectDocRef, newProject);
-      }
-      if (previousProjectDoc) {
-        transaction.set(previousProjectDoc.ref, previousProject);
-      }
-    });
-  } catch (error) {
-    console.error('Error upserting expense', error);
-  }
-
-  return newExpense;
-}
-
-const remove = async (expense: GeneralExpenseOld) => {
-  const { dayBookDocRef, expenseDocRef, projectDocRef } =
-    docReferencer(expense);
-
-  await runTransaction(DB, async (transaction) => {
-    // Read operations
-    const storedProjectDoc =
-      projectDocRef && (await transaction.get(projectDocRef));
-
-    // Data manipulation
-    let newProject: Project = PROJECTS_INITIAL_VALUE;
-    if (projectDocRef && storedProjectDoc?.exists()) {
-      newProject = storedProjectDoc?.data();
-      newProject.received_vouchers = newProject.received_vouchers.filter(
-        (voucher) => voucher.id !== expenseDocRef.id,
-      );
-    }
-
-    // Set operations
-    if (projectDocRef && storedProjectDoc?.exists()) {
-      transaction.set(projectDocRef, newProject);
-    }
-
-    // Delete operations
-    transaction.delete(dayBookDocRef);
-    transaction.delete(expenseDocRef);
-  });
-
-  return expense.id;
-};
-
 export const Expenses = {
   listByType,
-  upsert,
-  remove,
 };
