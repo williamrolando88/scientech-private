ffae0249ee9760f4e47e23b8cda4c36c3748aac3
William Morales <william.rolando88@hotmail.com>
2024-12-28 11:18:07 -0500

    Fix daybookTransaction converter

--- src/pages/dashboard/admin/index.tsx
+++ src/pages/dashboard/admin/index.tsx
@@ -1,9 +1,9 @@
 import { LoadingButton } from '@mui/lab';
-import { Button, Stack } from '@mui/material';
-import { useListDayBookTransactions } from '@src/hooks/cache/dayBook';
+import { Stack, Typography } from '@mui/material';
 import { COLLECTIONS_ENUM } from '@src/lib/enums/collections';
 import { DB } from '@src/settings/firebase';
 import { DayBookTransactionOld } from '@src/types/dayBook';
+import { DocumentRef } from '@src/types/documentReference';
 import {
   DoubleEntryAccounting,
   DoubleEntryAccountingTransaction,
@@ -24,9 +24,11 @@ import {
   writeBatch,
 } from 'firebase/firestore';
 import { useSnackbar } from 'notistack';
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 import DashboardLayout from 'src/components/shared/layouts/dashboard/DashboardLayout';
 import DashboardTemplate from 'src/components/shared/layouts/dashboard/DashboardTemplate';
+
+const BATCH_LIMIT = 500; // Firestore batch write limit
 
 const customPayment2Purchase = (data: CustomsPaymentOld): Purchase => {
   const ref: Record<string, string> = {};
@@ -162,10 +164,12 @@ const ExportExpenses: FC = () => {
   };
 
   const convertExpense2Purchase = () => {
-    enqueueSnackbar('Starting converting expenses to purchases');
     setConverting(true);
 
     const purchasesArray = new Array(expenses.length);
+
+    // TODO: Update the expense account to avoid having a default account when possible
+    // TODO: Fix the nonexisting expense default account
 
     expenses.forEach((expense, index) => {
       if (expense.type === 'non_deductible') {
@@ -194,7 +198,7 @@ const ExportExpenses: FC = () => {
 
     const errorsArray = purchasesArray.filter((p) => !p);
 
-    if (errorsArray) {
+    if (errorsArray.length) {
       enqueueSnackbar(`Finished with errors ${errorsArray.length}`);
     } else {
       enqueueSnackbar('Finished with no errors');
@@ -206,201 +210,261 @@ const ExportExpenses: FC = () => {
 
   const storePurchases = async () => {
     setUploadingExpenses(true);
-    enqueueSnackbar('Documents upload started');
-    const batch = writeBatch(DB);
 
-    purchases.forEach((document) => {
-      const docRef = doc(DB, COLLECTIONS_ENUM.PURCHASES, document.id ?? '');
+    const chunkedPurchases = []; // To store chunks of purchases
 
-      batch.set(docRef, document);
-      console.info('Added document with ID: ', docRef.id, ' to batch');
-    });
+    // Chunk the purchases array into groups of 500
+    for (let i = 0; i < purchases.length; i += BATCH_LIMIT) {
+      chunkedPurchases.push(purchases.slice(i, i + BATCH_LIMIT));
+    }
 
-    console.info('Committing batch');
-    await batch.commit();
-    console.info('Batch committed');
+    try {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const chunk of chunkedPurchases) {
+        const batch = writeBatch(DB);
 
-    enqueueSnackbar('Documents upload finished');
-    setUploadingExpenses(false);
+        chunk.forEach((document) => {
+          const docRef = doc(DB, COLLECTIONS_ENUM.PURCHASES, document.id ?? '');
+          batch.set(docRef, document);
+          console.info('Added document with ID: ', docRef.id, ' to batch');
+        });
+
+        console.info('Committing batch');
+        // eslint-disable-next-line no-await-in-loop
+        await batch.commit(); // Commit the current batch
+        console.info('Batch committed');
+      }
+
+      enqueueSnackbar('Documents upload finished');
+    } catch (error) {
+      console.error('Error committing batch:', error);
+      enqueueSnackbar('Error uploading documents', { variant: 'error' });
+    } finally {
+      setUploadingExpenses(false);
+    }
   };
 
   return (
-    <Stack direction="row" gap={2}>
-      <LoadingButton onClick={loadExpenses} loading={loadingExpenses}>
-        Load Expenses into memory
-      </LoadingButton>
-
-      <LoadingButton
-        onClick={convertExpense2Purchase}
-        disabled={expenses.length === 0}
-        loading={converting}
-      >
-        Convert Expenses into Purchases
-      </LoadingButton>
-
-      <LoadingButton
-        onClick={storePurchases}
-        disabled={purchases.length === 0}
-        loading={uploadingExpenses}
-      >
-        Store Purchases
-      </LoadingButton>
+    <Stack>
+      <Typography variant="h4">Convert Expenses to Purchases</Typography>
+      <Stack direction="row" gap={2}>
+        <LoadingButton onClick={loadExpenses} loading={loadingExpenses}>
+          Load Expenses into memory
+        </LoadingButton>
+        <LoadingButton
+          onClick={convertExpense2Purchase}
+          disabled={expenses.length === 0}
+          loading={converting}
+        >
+          Convert Expenses into Purchases
+        </LoadingButton>
+        <LoadingButton
+          onClick={storePurchases}
+          disabled={purchases.length === 0}
+          loading={uploadingExpenses}
+        >
+          Store Purchases
+        </LoadingButton>
+      </Stack>
     </Stack>
   );
 };
 
-const existingProjects: Record<string, string> = {
-  885: 'nOPC8LoXorcemFVO8ZcZ',
-  809: 'gsrd25reZ4ziIQYSLelz',
-  877: 'fUeg7iVoS62Z9I2tfw5W',
-  673: 'Q7rkr4Cen5aJieFf3Brk',
-  896: 'NJqGL2fDDKW28QCzvMHN',
-  829: 'M68lnROy56aCUgiog214',
+const getRefMapper = (oldData: DayBookTransactionOld[]) => {
+  const existingProjects: Record<string, string> = {
+    885: 'nOPC8LoXorcemFVO8ZcZ',
+    809: 'gsrd25reZ4ziIQYSLelz',
+    877: 'fUeg7iVoS62Z9I2tfw5W',
+    673: 'Q7rkr4Cen5aJieFf3Brk',
+    896: 'NJqGL2fDDKW28QCzvMHN',
+    829: 'M68lnROy56aCUgiog214',
+  };
+
+  const refMap: Record<string, DocumentRef> = {};
+
+  oldData.forEach((data) =>
+    data.transactions.forEach(async (details) => {
+      if (!data.id) return;
+
+      if (details.project_id) {
+        refMap[String(data.id)] = {
+          ...(refMap[String(data.id)] ?? {}),
+          projectId: details.project_id!,
+        };
+      }
+
+      if (details.expense_id) {
+        refMap[String(data.id)] = {
+          ...(refMap[String(data.id)] ?? {}),
+          purchaseId: details.expense_id!,
+        };
+      }
+
+      const quotation_id = String(details.quotation_id);
+
+      if (
+        quotation_id &&
+        Object.keys(existingProjects).includes(quotation_id)
+      ) {
+        refMap[String(data.id)] = {
+          ...(refMap[String(data.id)] ?? {}),
+          projectId: existingProjects[quotation_id] as string,
+        };
+      }
+    })
+  );
+
+  return refMap;
 };
 
-const ExportDayBookNewFormat: FC = () => {
-  const { data, isLoading } = useListDayBookTransactions();
-  const [linkedTransactions, setLinkedTransactions] = useState<
-    Record<string, Record<string, string>>
-  >({});
+const DayBookTransaction2DoubleEntryAccounting = (
+  data: DayBookTransactionOld,
+  refMap: Record<string, DocumentRef>
+): DoubleEntryAccounting => {
+  const transactions: DoubleEntryAccountingTransaction[] =
+    data.transactions.map((transaction) => ({
+      accountId: transaction.account_id,
+      credit: transaction.credit || 0,
+      debit: transaction.debit || 0,
+    }));
 
-  useEffect(() => {
-    if (data.length) {
-      Promise.all(
-        data.map(async (transaction) => {
-          Promise.all(
-            transaction.transactions.map(async (details) => {
-              if (transaction.id) {
-                if (details.project_id) {
-                  setLinkedTransactions((prev) => ({
-                    ...prev,
-                    [String(transaction.id)]: {
-                      ...(prev[String(transaction.id)] ?? {}),
-                      projectId: details.project_id!,
-                    },
-                  }));
-                }
+  const getDocId = (oldId: string): string => {
+    const refObject = refMap[oldId];
 
-                if (details.expense_id) {
-                  setLinkedTransactions((prev) => ({
-                    ...prev,
-                    [String(transaction.id)]: {
-                      ...(prev[String(transaction.id)] ?? {}),
-                      purchaseId: details.expense_id!,
-                    },
-                  }));
-                }
-
-                const quotation_id = String(details.quotation_id);
-
-                if (
-                  quotation_id &&
-                  Object.keys(existingProjects).includes(quotation_id)
-                ) {
-                  setLinkedTransactions((prev) => ({
-                    ...prev,
-                    [String(transaction.id)]: {
-                      ...(prev[String(transaction.id)] ?? {}),
-                      projectId: existingProjects[quotation_id] as string,
-                    },
-                  }));
-                }
-              }
-            })
-          );
-        })
-      );
-    }
-  }, [data]);
-
-  const converter = (oldData: DayBookTransactionOld): DoubleEntryAccounting => {
-    const transactions: DoubleEntryAccountingTransaction[] =
-      oldData.transactions.map((transaction) => ({
-        accountId: transaction.account_id,
-        credit: transaction.credit || 0,
-        debit: transaction.debit || 0,
-      }));
-
-    const getDocId = (oldId: string): string => {
-      const refObject = linkedTransactions[oldId];
-
-      if (!refObject) {
-        return oldId;
-      }
-
-      const hasNonProjectId = Object.keys(refObject).filter(
-        (k) => k !== 'projectId'
-      );
-
-      if (hasNonProjectId.length) {
-        return linkedTransactions[oldId][hasNonProjectId[0]];
-      }
-
+    if (!refObject) {
       return oldId;
-    };
-
-    return {
-      id: getDocId(oldData.id!),
-      issueDate: oldData.createdAt || new Date(),
-      description: oldData.transactions[0].description || '',
-      ref: linkedTransactions[oldData.id!] || {},
-      transactions,
-      accounts: oldData.transactions.map((t) => t.account_id),
-      locked: oldData.locked || false,
-      createdAt: oldData.createdAt || new Date(),
-      updatedAt: oldData.updatedAt || new Date(),
-    };
-  };
-
-  const execute = async () => {
-    if (Object.keys(linkedTransactions).length === 0) {
-      alert('Cargando datos...');
-      return;
     }
 
-    console.log('Started batch creation');
-    const batch = writeBatch(DB);
+    const hasNonProjectId = Object.keys(refObject).filter(
+      (k) => k !== 'projectId'
+    );
 
-    data.forEach((document) => {
-      const docRef = doc(
-        DB,
-        COLLECTIONS_ENUM.DOUBLE_ENTRY_ACCOUNTING,
-        document.id ?? ''
-      );
+    if (hasNonProjectId.length) {
+      // @ts-expect-error - There is an existing id
+      return refMap[oldId][hasNonProjectId[0]] as string;
+    }
 
-      const isValid = document.transactions.every(
-        (transaction) => transaction.account_id
-      );
-
-      if (!isValid) {
-        console.error('Invalid transaction data', document);
-
-        return;
-      }
-
-      const convertedData = converter(document);
-      batch.set(docRef, convertedData);
-      console.log('Added document with ID: ', docRef.id, ' to batch');
-    });
-
-    console.log('Committing batch');
-    await batch.commit();
-    console.log('Batch committed');
+    return oldId;
   };
 
-  if (isLoading) {
-    return <p>Cargando datos...</p>;
-  }
+  return {
+    id: getDocId(data.id ?? ''),
+    description: data.transactions[0]?.description ?? '',
+    ref: refMap[data.id ?? ''] || {},
+    transactions,
+    accounts: transactions.map((t) => t.accountId),
+    locked: data.locked || false,
+    createdAt: data.createdAt || new Date(),
+    updatedAt: data.updatedAt || new Date(),
+    issueDate: data.createdAt || new Date(),
+  };
+};
 
+const ExportDoubleEntryAccounting: FC = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  const [loadingOldData, setLoadingOldData] = useState(false);
+  const [converting, setConverting] = useState(false);
+  const [uploadingData, setUploadingData] = useState(false);
+  const [oldRecords, setOldRecords] = useState<DayBookTransactionOld[]>([]);
+  const [newEntries, setNewEntries] = useState<DoubleEntryAccounting[]>([]);
+
+  const fetchOldRecords = async () => {
+    setLoadingOldData(true);
+    const q = query(collection(DB, COLLECTIONS_ENUM.DAY_BOOK_TRANSACTIONS));
+
+    const querySnapshot = await getDocs(q);
+
+    const oldDataArray = querySnapshot.docs.map(
+      (d) => d.data() as DayBookTransactionOld
+    );
+    setOldRecords(oldDataArray);
+    setLoadingOldData(false);
+    enqueueSnackbar(`${oldDataArray.length} were loaded into memory`);
+  };
+
+  const convertEntries = () => {
+    setConverting(true);
+
+    const refMap = getRefMapper(oldRecords);
+
+    try {
+      const newRecords = oldRecords.map((r) =>
+        DayBookTransaction2DoubleEntryAccounting(r, refMap)
+      );
+
+      setNewEntries(newRecords);
+      enqueueSnackbar('Finished entries convertion');
+    } catch (e) {
+      enqueueSnackbar(`Something went wrong`);
+      console.error(e);
+    } finally {
+      setConverting(false);
+    }
+  };
+
+  const storeNewEntries = async () => {
+    setUploadingData(true);
+
+    const chunkedEntries = [];
+
+    for (let i = 0; i < newEntries.length; i += BATCH_LIMIT) {
+      chunkedEntries.push(newEntries.slice(i, i + BATCH_LIMIT));
+    }
+
+    try {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const chunk of chunkedEntries) {
+        const batch = writeBatch(DB);
+
+        chunk.forEach((document) => {
+          const docRef = doc(
+            DB,
+            COLLECTIONS_ENUM.DOUBLE_ENTRY_ACCOUNTING,
+            document.id ?? ''
+          );
+          batch.set(docRef, document);
+          console.info('Added document with ID: ', docRef.id, ' to batch');
+        });
+
+        console.info('Committing batch');
+        // eslint-disable-next-line no-await-in-loop
+        await batch.commit(); // Commit the current batch
+        console.info('Batch committed');
+      }
+
+      enqueueSnackbar('Documents upload finished');
+    } catch (error) {
+      console.error('Error committing batch:', error);
+      enqueueSnackbar('Error uploading documents', { variant: 'error' });
+    } finally {
+      setUploadingData(false);
+    }
+  };
   return (
-    <Button
-      onClick={execute}
-      disabled={isLoading || !Object.keys(linkedTransactions).length}
-    >
-      {!Object.keys(linkedTransactions).length
-        ? 'No hay datos'
-        : 'Exportar libro diario en Nuevo Formato'}
-    </Button>
+    <Stack>
+      <Typography variant="h4">Convert daybook transactions</Typography>
+      <Stack direction="row" gap={2}>
+        <LoadingButton onClick={fetchOldRecords} loading={loadingOldData}>
+          Load old transactions into Memory
+        </LoadingButton>
+
+        <LoadingButton
+          onClick={convertEntries}
+          loading={converting}
+          disabled={oldRecords.length === 0}
+        >
+          Convert into new format
+        </LoadingButton>
+
+        <LoadingButton
+          onClick={storeNewEntries}
+          loading={uploadingData}
+          disabled={newEntries.length === 0}
+        >
+          Storing new format
+        </LoadingButton>
+      </Stack>
+    </Stack>
   );
 };
 
@@ -412,8 +476,8 @@ export default function Page() {
   return (
     <DashboardTemplate documentTitle="Admin" heading="Admin Tools">
       <Stack>
+        <ExportDoubleEntryAccounting />
         <ExportExpenses />
-        <ExportDayBookNewFormat />
       </Stack>
     </DashboardTemplate>
   );
