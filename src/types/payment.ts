f6569b1cadd1f02674ef73851c28c29465b0079b
William Morales <william.rolando88@hotmail.com>
2024-12-29 11:06:52 -0500

    Add paymentId key

--- src/types/payment.ts
+++
@@ -1,4 +0,0 @@
-import { PaymentSchema } from '@src/lib/schemas/payment';
-import { z } from 'zod';
-
-export type Payment = z.infer<typeof PaymentSchema>;
--- src/types/purchases.ts
+++ src/types/purchases.ts
@@ -1,6 +1,7 @@
 import {
   CustomsPaymentSchema,
   NonDeductibleSchema,
+  PaymentSchema,
   PurchaseSchema,
   ReceivedInvoiceSchema,
   SaleNoteSchema,
@@ -11,4 +12,5 @@ export type ReceivedInvoice = z.infer<typeof ReceivedInvoiceSchema>;
 export type CustomsPayment = z.infer<typeof CustomsPaymentSchema>;
 export type NonDeductible = z.infer<typeof NonDeductibleSchema>;
 export type SaleNote = z.infer<typeof SaleNoteSchema>;
+export type Payment = z.infer<typeof PaymentSchema>;
 export type Purchase = z.infer<typeof PurchaseSchema>;
